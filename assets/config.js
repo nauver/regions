55c461dddb96f67836dd115a030517f3c43c48ea
nauver <65015444+nauver@users.noreply.github.com>
2026-06-26 16:23:45 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   SUPABASE_URL: 'https://imkkzdfvqzflhvcfbsld.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_6BmvPJCXd_0NgYKPpkg9fWQ_a7nh81H5',
+  SUPABASE_ANON_KEY: 'sb_publishable_6BmvPJCXd_0NgYKPgk9fWQ_a7nh81H5',
   STORAGE_BUCKET: 'participant-photos',
   DEMO_MODE: false,
   POLL_MS: 2000,
